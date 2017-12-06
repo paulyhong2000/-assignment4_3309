74a9731a87bab944eff1a69225c32aa1fabd51e5
DwHong <dhong45@uwo.ca>
2017-12-06 21:57:53 +0000

    done

--- src/homepage.js
+++ src/homepage.js
@@ -187,3 +187,20 @@ function exitbutton()
   window.close();
 }
 
+function isNumberKey(txt, evt) {
+
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode == 46) {
+        //Check if the text already contains the . character
+        if (txt.value.indexOf('.') === -1) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (charCode > 31
+             && (charCode < 48 || charCode > 57))
+            return false;
+    }
+    return true;
+}
